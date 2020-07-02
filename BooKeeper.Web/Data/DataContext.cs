b079c242091d273e349c215947f47a61a0273b52
CristinaMontillaMoya <cristinamontilla.90@gmail.com>
2020-07-02 13:00:28 +0200

    DB conection configure

---
+++ BooKeeper.Web/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooKeeper.Web.Data
+{
+    using BooKeeper.Web.Data.Entities;
+    using Microsoft.EntityFrameworkCore;
+
+    public class DataContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ BooKeeper.Web/Data/Entities/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BooKeeper.Web.Data.Entities
+{
+    public class Book
+    {
+        public string Isbn { get; set; }
+        public int IdCategory { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime Date { get; set; }
+        public string Synopsis { get; set; }
+
+        [Display(Name ="Image")]
+        public string Image { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode =false)]
+        public float Price { get; set; }
+
+        public int Stock { get; set; }
+    }
+}
