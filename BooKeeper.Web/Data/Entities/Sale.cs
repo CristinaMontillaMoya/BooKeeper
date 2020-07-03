0fb42141363b91e8ba46e1268b5f28f48498cff1
DCS91 <dcuencasantamaria.sanjose@alumnado.fundacionloyola.net>
2020-07-03 13:49:42 +0200

    Add Sale, Sale Detail Entity

---
+++ BooKeeper.Web/Data/Entities/Sale.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BooKeeper.Web.Data.Entities
+{
+    public class Sale
+    {
+        public int Id { get; set; }
+        public List<SaleDetail> SaleDetail { get; set; }
+        public DateTime Date { get; set; }
+        public string Country { get; set; }
+        public string Province { get; set; }
+        public int IdUser { get; set; }
+        public string telephone { get; set; }
+        public string DeliveryData { get; set; }
+
+    }
+}
