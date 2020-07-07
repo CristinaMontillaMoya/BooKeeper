24a63fff613726bc6259a690e0d90349f4c04f1e
CristinaMontillaMoya <cristinamontilla.90@gmail.com>
2020-07-07 11:08:13 +0200

    Fixing db design and testing app

--- BooKeeper.Web/Controllers/HomeController.cs
+++ BooKeeper.Web/Controllers/HomeController.cs
@@ -6,20 +6,53 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using BooKeeper.Web.Models;
+using BooKeeper.Web.Data.Entities;
+using BooKeeper.Web.Data;
 
 namespace BooKeeper.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        DataContext cntx;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, DataContext cntx)
         {
             _logger = logger;
+            this.cntx = cntx;
         }
 
         public IActionResult Index()
         {
+            Category test = new Category()
+            {
+                Name = "Terror"
+            };
+        Book testBook = new Book()
+        {
+            Isbn = "123124hb4j3bjh",
+            Title = "Prueba",
+            Author = "Autor",
+            Date = DateTime.Now,
+            Synopsis = null,
+            Image = null,
+            Price = 10,
+            IdCategory = 1,
+            Stock = 1
+
+        };
+
+            cntx.Categories.Add(test);
+            if (cntx.SaveChanges() == 1)
+            {
+                Console.WriteLine(testBook.Author);
+                cntx.Books.Add(testBook);
+                cntx.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine(testBook.Category);
+            }
             return View();
         }
 
