775c09b4c506afedf3d3b03263391115ad1a961e
DCS91 <dcuencasantamaria.sanjose@alumnado.fundacionloyola.net>
2020-07-21 14:08:12 +0200

    Fix errorText

--- BooKeeper.UIForms/BooKeeper.UIForms/ViewModels/LoginViewModel.cs
+++ BooKeeper.UIForms/BooKeeper.UIForms/ViewModels/LoginViewModel.cs
@@ -9,7 +9,7 @@
     {
         public string Email { get; set; }
         public string Password { get; set; }
-        public ICommand LoginComamnd => new RelayCommand(Login);
+        public ICommand LoginCommand => new RelayCommand(Login);
 
         private async void Login()
         {
