6fedfd028ce2424d2502c7baf0737c71d7e50f52
DCS91 <dcuencasantamaria.sanjose@alumnado.fundacionloyola.net>
2020-08-04 12:07:08 +0200

    Implement changeUser and changePassword

---
+++ BooKeeper.Web/Models/ChangeUserViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace BooKeeper.Web.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    public class ChangeUserViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string Surname { get; set; }
+    }
+}
